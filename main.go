d516161aa73cfed8d383218300a0645d5b017121
FutileCode <99463618+FutileCode@users.noreply.github.com>
2022-08-14 22:40:07 +0100

    Add files via upload

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	totalTime := flag.String("time", "", "The length of the timer | e.g. 3h, 25m, 1h30m, 45s")
+
+	flag.Parse()
+
+	if *totalTime == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	v, _ := time.ParseDuration(*totalTime)
+
+	formattedTime := time.Now().Add(v).Format("2006-01-02T15:04:05") + "+01:00"
+
+	parsedTime, _ := time.Parse(time.RFC3339, formattedTime)
+
+	fmt.Printf("Staring timer for %v \n", v)
+
+	timeLeft := getTimeLeft(parsedTime)
+	fmt.Printf("%02d : %02d : %02d : %02d\n", timeLeft.d, timeLeft.h, timeLeft.m, timeLeft.s)
+
+	for range time.Tick(1 * time.Second) {
+		timeLeft := getTimeLeft(parsedTime)
+
+		if timeLeft.t <= 0 {
+			fmt.Println(strings.Repeat("-", 30))
+			fmt.Println("Time ended")
+			fmt.Println(strings.Repeat("-", 30))
+			os.Exit(1)
+		}
+
+		fmt.Printf("%02d : %02d : %02d : %02d\n", timeLeft.d, timeLeft.h, timeLeft.m, timeLeft.s)
+	}
+}
+
+type countdown struct {
+	t int
+	d int
+	h int
+	m int
+	s int
+}
+
+func getTimeLeft(t time.Time) countdown {
+	timeNow := time.Now()
+	left := t.Sub(timeNow)
+
+	total := int(left.Seconds())
+	days := int(total / (60 * 60 * 24))
+	hours := int(total / (60 * 60) % 24)
+	minutes := int(total/60) % 60
+	seconds := int(total % 60)
+
+	return countdown{
+		t: total,
+		d: days,
+		h: hours,
+		m: minutes,
+		s: seconds,
+	}
+
+}
